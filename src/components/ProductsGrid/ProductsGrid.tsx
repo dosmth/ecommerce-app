ed01ac45014ebf7adceadbae57bbbc4121ab985d
dosmth <imusedto@gmail.com>
2021-05-05 02:19:18 +0300

    ProductGrid TS migration

--- src/components/ProductsGrid/ProductsGrid.tsx
+++ src/components/ProductsGrid/ProductsGrid.tsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react";
-import styles from "./ProductsGrid.css";
+import * as React from "react";
+import styles from "./ProductsGrid.module.css";
 import Card from "../ProductCard/ProductCard";
 
-export default function ProductsGrid(props) {
+type Props = {
+  products: { id: number; title: string; img: string; price: number }[];
+};
+
+export default function ProductsGrid(props: Props) {
   let data = Array.isArray(props.products) ? props.products : [];
   const products = data.map((d) => (
     <Card key={d.id} id={d.id} title={d.title} img={d.img} price={d.price} />
